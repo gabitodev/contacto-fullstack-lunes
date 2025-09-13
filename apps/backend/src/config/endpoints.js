056b95dc070702141110c7e83374d3148ab43161
gabitodev <gabitodev@gmail.com>
2025-09-13 10:05:28 -0400

    feat: add endpoint configuration and update verification email link

---
+++ apps/backend/src/config/endpoints.js
@@ -0,0 +1,4 @@
+export const endpoint =
+  process.env.NODE_ENV === 'production'
+    ? 'https://contacto-fullstack-lunes.onrender.com'
+    : 'http://localhost:3000';
--- apps/backend/src/modules/users/users.routes.js
+++ apps/backend/src/modules/users/users.routes.js
@@ -4,6 +4,7 @@ import bcrypt from 'bcrypt';
 import usersRepository from './users.repository.js';
 import jwt from 'jsonwebtoken';
 import nodemailerService from '../../services/nodemailer.js';
+import { endpoint } from '../../config/endpoints.js';
 const usersRouter = express.Router();
 
 usersRouter.post('/', async (req, res) => {
@@ -19,7 +20,7 @@ usersRouter.post('/', async (req, res) => {
     from: process.env.EMAIL_USER,
     to: body.email,
     subject: 'Verifica tu correo',
-    html: `<a href="http://localhost:4321/verify/${token}">Verifica tu correo</a>`,
+    html: `<a href="${endpoint}/verify/${token}">Verifica tu correo</a>`,
   });
 
   res.sendStatus(200);
